6e18c940af68c79d6338260fd568f4fe5b2a8985
Vincent Xu <vincent.xu1@outlook.com>
2025-07-16 14:38:02 -0700

    Completed assignment 2.4.3

--- Program.cs
+++ Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            (double x, double y) coordinate;
+
+            Console.Write("Input the value for X coordinate: ");
+            coordinate.x = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Input the value for Y coordinate: ");
+            coordinate.y = Convert.ToDouble(Console.ReadLine());
+
+            string quadrant = DetermineQuadrant(coordinate);
+            Console.WriteLine($"The coordinate point {coordinate} lies in { (quadrant == "" ? "no" : "the " + quadrant) } quadrant.");
+        }
+
+        static string DetermineQuadrant((double x, double y) coordinate)
+        {
+            if (coordinate.x > 0 && coordinate.y > 0)
+                return "First";
+            else if (coordinate.x < 0 && coordinate.y > 0)
+                return "Second";
+            else if (coordinate.x < 0 && coordinate.y < 0)
+                return "Third";
+            else if (coordinate.x > 0 && coordinate.y < 0)
+                return "Fourth";
+            else
+                return "";
         }
     }
 }
